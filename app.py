adcf23ab9382944578e0d809cd080acc06e468e0
Gabe <gabe1118@yahoo.com>
2016-04-16 15:14:35 -0400

    Init Flask Hello World

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from flask import Flask, render_template, session, request
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ makefile
@@ -0,0 +1,2 @@
+all:
+	python game.py
