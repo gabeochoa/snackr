eba339b1c03b34b06a9bae5c5f663bf587380f54
Gabe <gabe1118@yahoo.com>
2016-04-16 20:18:52 -0400

    add port

--- app.py
+++ app.py
@@ -24,5 +24,6 @@ def inventory(database = mongoconn.openDB()):
     print(inv)
     return render_template('inventory.html', inv = inv)
 
+port = int(os.environ.get('PORT', 5000))
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port)
