b992ad729331c4feaa8fc47ec740ffd296421391
Gabe <gabe1118@yahoo.com>
2016-04-16 20:34:18 -0400

    update host

--- app.py
+++ app.py
@@ -27,4 +27,4 @@ def inventory(database = mongoconn.openDB()):
 
 port = int(os.environ.get('PORT', 5000))
 if __name__ == '__main__':
-    app.run(debug=True, port=port)
+    app.run(host='0.0.0.0', port=port)
